8b769c2b28696cfe22e182db320b88d4f9fcd45b
DavidTaplin <DTaplin616@live.com>
2022-10-23 21:59:13 -0500

    create a complex structure
    created a complex stucture of files nested children to parents and did very very basic css styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'create-complex-stucture';
+  title = 'create a complex stucture';
 }
